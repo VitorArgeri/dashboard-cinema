247a7d9e45689b51b3ad7a350c0d7874dc5c7acc
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-03-11 09:33:33 -0300

    feat: create sidebar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/header";
+import Sidebar from "../components/sidebar";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <main className={styles.dashboardContainer}>
+        <Sidebar />
+      </main>
     </div>
   );
 }
